79eff8a3ebe2aed8b8359589571725e92cfe588a
k-technical <kbat292@gmail.com>
2026-04-27 12:50:17 +0300

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,42 @@
+function downloadSVG(svgContent, filename = 'result.svg') {
+    if (!svgContent.startsWith('<?xml')) {
+        svgContent = '<?xml version="1.0" encoding="utf-8"?>\n' + svgContent;
+    }
+    
+    const blob = new Blob([svgContent], { type: 'image/svg+xml' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+
+function extractJSON(input) {
+    try {
+        return JSON.parse(input);
+    } catch (e) {
+        try {
+            let cleaned = input.replace(/^"|"$/g, '').replace(/\\"/g, '"');
+            return JSON.parse(cleaned);
+        } catch (e2) {
+            throw new Error('Невалидный JSON');
+        }
+    }
+}
+
+function formatSeatId(row, place) {
+    return `Ряд_x5F_${row}_x7C_${place}-${place}`;
+}
+
+function parseSVG(svgString) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(svgString, 'image/svg+xml');
+    const error = doc.querySelector('parsererror');
+    if (error) {
+        throw new Error('Невалидный SVG');
+    }
+    return doc;
+}
